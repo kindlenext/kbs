4543fac0bb4ea16ae02ec30952c3ad4e971a0e78
shoaibv2 <skrj1337@gmail.com>
2022-04-13 22:01:05 +0500

    add Toasts(#19)

---
+++ apps/views/pages/Toast.tsx
@@ -0,0 +1,13 @@
+import { Toasts } from "kbs-core";
+
+function Toast() {
+  return (
+    <Toasts
+      title="Boostrap"
+      time="2 min ago"
+      label="Hello, world! This is a toast message."
+    />
+  );
+}
+
+export default Toast;
--- packages/kbs-core/src/index.tsx
+++ packages/kbs-core/src/index.tsx
@@ -1,2 +1,4 @@
 import * as React from "react";
+import Toasts from "./components/Toast";
 export { Button, type ButtonProps } from "./Button";
+export { Toasts };
