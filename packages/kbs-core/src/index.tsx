7b8aa2b70f60ba07015ede5498001e4e3bba84af
Nadia Iqbal <nadiaiqbal2600@example.com>
2022-04-24 22:52:55 +0500

    Add Card Basic Component in kbs (#6)

--- packages/kbs-core/src/index.tsx
+++ packages/kbs-core/src/index.tsx
@@ -1,16 +1,16 @@
-import CardTextAlignment from "./components/Cardalign";
-import CardBackgroundColour from "./components/Cardbgclr";
-import CardBody from "./components/Cardbody";
-import CardBorder from "./components/Cardborder";
-import CardExample from "./components/Cardexample";
-import CardHeaderFooter from "./components/Cardheaderfooter";
-import CardHorizontal from "./components/Cardhorizontal";
-import CardImage from "./components/Cardimage";
-import CardImageOverlay from "./components/Cardimgoverlay";
-import CardKitchenSink from "./components/Cardkitchensink";
-import CardLinks from "./components/Cardlinks";
-import CardListGroups from "./components/Cardlistgr";
-import CardNavItems from "./components/Cardnav";
+import CardTextAlignment from "../../../apps/views/components/Cardalign";
+import CardBackgroundColour from "../../../apps/views/components/Cardbgclr";
+import CardBody from "../../../apps/views/components/Cardbody";
+import CardBorder from "../../../apps/views/components/Cardborder";
+import CardExample from "../../../apps/views/components/Cardexample";
+import CardHeaderFooter from "../../../apps/views/components/Cardheaderfooter";
+import CardHorizontal from "../../../apps/views/components/Cardhorizontal";
+import CardImage from "../../../apps/views/components/Cardimage";
+import CardImageOverlay from "../../../apps/views/components/Cardimgoverlay";
+import CardKitchenSink from "../../../apps/views/components/Cardkitchensink";
+import CardLinks from "../../../apps/views/components/Cardlinks";
+import CardListGroups from "../../../apps/views/components/Cardlistgr";
+import CardNavItems from "../../../apps/views/components/Cardnav";
 
 export { Button, type ButtonProps } from "./Button";
 export {
